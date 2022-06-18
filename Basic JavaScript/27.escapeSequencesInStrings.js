1bfeec09336c8fda46133303a8a2504a1bdab96c
s.zavadski <s.zavadski@affise.com>
2022-06-18 09:58:39 +0300

    camp: 27-31

---
+++ Basic JavaScript/27.escapeSequencesInStrings.js
@@ -0,0 +1,17 @@
+// ! Assign the following three lines of text into the single variable myStr using escape sequences.
+// FirstLine
+//     \SecondLine
+// ThirdLine
+
+// const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+// console.log(myStr);
+
+// ? tip
+// \'	single quote
+// \"	double quote
+// \\	backslash
+// \n	newline
+// \r	carriage return
+// \t	tab
+// \b	word boundary
+// \f	form feed
---
+++ Basic JavaScript/30.constructingStringsWithVariables.js
@@ -0,0 +1,6 @@
+// ! Set myName to a string equal to your name and build myStr with myName between the strings My name is and and I am well!
+
+// Only change code below this line
+const myName = "Siarhei";
+const myStr = "My name is " + myName + " and and I am well!";
+console.log(myStr);
