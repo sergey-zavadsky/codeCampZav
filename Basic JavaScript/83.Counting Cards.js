c9db41c8e7dc20eaea79cd54f5e593fd445e2db0
s.zavadski <s.zavadski@affise.com>
2022-07-07 23:49:46 +0300

    codecamp: 83-87

--- Basic JavaScript/83.Counting Cards.js
+++ Basic JavaScript/83.Counting Cards.js
@@ -2,13 +2,39 @@ let count = 0;
 
 function cc(card) {
 	// Only change code below this line
-
+	switch (card) {
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			count++;
+			break;
+		case 7:
+		case 8:
+		case 9:
+			count;
+			break;
+		case 10:
+		case "J":
+		case "Q":
+		case "K":
+		case "A":
+			count--;
+			break;
+	}
+	if (count > 0) {
+		return count + " Bet";
+	}
+	if (count < 0 || count === 0) {
+		return count + " Hold";
+	}
 	return "Change Me";
 	// Only change code above this line
 }
 
-cc(2);
-cc(3);
-cc(7);
-cc("K");
-cc("A");
+console.log(cc(2));
+console.log(cc(3));
+console.log(cc(4));
+console.log(cc(5));
+console.log(cc(6));
---
+++ Basic JavaScript/84.Build JavaScript Objects.js
@@ -0,0 +1,8 @@
+const myDog = {
+	// Only change code below this line
+	name: "Peter",
+	legs: 3,
+	tails: 1,
+	friends: ["cat", "parrot"],
+	// Only change code above this line
+};
