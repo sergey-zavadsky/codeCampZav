b7b6746479a6b583307aae6ab53ed4535e053bf1
s.zavadski <s.zavadski@affise.com>
2022-07-12 21:07:14 +0300

    refactor: 102

--- Basic JavaScript/102.Nesting For Loops.js
+++ Basic JavaScript/102.Nesting For Loops.js
@@ -1,20 +1,11 @@
 function multiplyAll(arr) {
 	let product = 1;
-	// Only change code below this line
-	let c;
-	let d = [];
 	for (let i = 0; i < arr.length; i++) {
-		for (let b = 0; b < arr.length; b++) {
-			if (arr[i][b]) {
-				c = arr[i][b];
-				d.push(c);
-			}
+		for (let b = 0; b < arr[i].length; b++) {
+			product = product * arr[i][b];
 		}
 	}
-	for (let i = 0; i < d.length; i++) {
-		product *= d[i];
-	}
-	// Only change code above this line
+	console.log(product);
 	return product;
 }
 
@@ -23,3 +14,21 @@ multiplyAll([
 	[3, 4],
 	[5, 6, 7],
 ]);
+
+// function multiplyAll(arr) {
+// 	let product = 1;
+// 	let c;
+// 	let d = [];
+// 	for (let i = 0; i < arr.length; i++) {
+// 		for (let b = 0; b < arr.length; b++) {
+// 			if (arr[i][b]) {
+// 				c = arr[i][b];
+// 				d.push(c);
+// 			}
+// 		}
+// 	}
+// 	for (let i = 0; i < d.length; i++) {
+// 		product *= d[i];
+// 	}
+// 	return product;
+// }
